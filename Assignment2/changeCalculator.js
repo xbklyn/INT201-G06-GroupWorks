3421cf355e5bffcefdba472255f4b2629d4bc6f5
tan <t.songglod@gmail.com>
2021-08-31 20:53:38 +0700

    [EDIT] adjust lines

--- Assignment2/changeCalculator.js
+++ Assignment2/changeCalculator.js
@@ -1,46 +1,44 @@
 const Notes = {         //สร้าง object notes เพื่อเก็ฐค่าแบงค์และจำนวนแบงค์ที่มีอยู่
-     "500-Notes" : 0,
-     "100-Notes" : 0,
-     "50-Notes" : 0,
-     "20-Notes" : 0,
-     "10-Notes" : 0,
-     "5-Notes" : 0,
-     "2-Notes" : 0,
-     "1-Notes" : 0,
-     add (key , value) {Notes[`${key}-Notes`] += value;}, //function สำหรับเพิ่มแบงค์ใน Notes
-     remove (key) {Notes[`${key}-Notes`] -= 1;}, //function สำหรับนำบงค์ออกจาก Notes
-     check (key) {return Notes[`${key}-Notes`] > 0;} //
+    "500-Notes": 0,
+    "100-Notes": 0,
+    "50-Notes": 0,
+    "20-Notes": 0,
+    "10-Notes": 0,
+    "5-Notes": 0,
+    "2-Notes": 0,
+    "1-Notes": 0,
+    add(key, value) { Notes[`${key}-Notes`] += value; }, //function สำหรับเพิ่มแบงค์ใน Notes
+    remove(key) { Notes[`${key}-Notes`] -= 1; }, //function สำหรับนำบงค์ออกจาก Notes
+    check(key) { return Notes[`${key}-Notes`] > 0; } //
 }
 
-function changeCalculation(totalPrice, moneyFromCustomer){
-    let changes = { change : moneyFromCustomer - totalPrice };
+function changeCalculation(totalPrice, moneyFromCustomer) {
+    let changes = { change: moneyFromCustomer - totalPrice };
     let totalChange = changes.change;
-    
-    let notes = [500,100,50,20,10,5,2];
-    for(let note of notes){
-        
-        if(Notes.check(note)){Notes.remove(note)} else return `Not enough changes.`
-        while(totalChange - note >= 0 ){
+
+    let notes = [500, 100, 50, 20, 10, 5, 2];
+    for (let note of notes) {
+        if (Notes.check(note)) { 
+            Notes.remove(note) 
+        } else return `Not enough changes.`
+        while (totalChange - note >= 0) {
             totalChange -= note;
-            changes[`${note}-Change`] = changes[`${note}-Change`] === undefined ? changes[`${note}-Change`] = 1 : changes[`${note}-Change`]+= 1; 
-            
+            changes[`${note}-Change`] = changes[`${note}-Change`] === undefined ? changes[`${note}-Change`] = 1 : changes[`${note}-Change`] += 1;
         }
-    
     }
-    if(totalChange > 0 ) changes[`1-Change`] = totalChange;
+    if (totalChange > 0) changes[`1-Change`] = totalChange;
     return changes
 }
 
-
-Notes.add(500 , 100);
-Notes.add(100 , 100);
-Notes.add(50 , 100);
-Notes.add(20 , 100);
-Notes.add(10 , 100);
-Notes.add(5 , 100);
-Notes.add(2 , 100);
-Notes.add(1 , 100);
+Notes.add(500, 100);
+Notes.add(100, 100);
+Notes.add(50, 100);
+Notes.add(20, 100);
+Notes.add(10, 100);
+Notes.add(5, 100);
+Notes.add(2, 100);
+Notes.add(1, 100);
 console.log(Notes);
 
-console.log(changeCalculation(1000,2000));
-console.log(changeCalculation(120,132));
+console.log(changeCalculation(1000, 2000));
+console.log(changeCalculation(120, 132));
