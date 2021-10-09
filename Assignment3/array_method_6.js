cde9a52e4b894b10b5391e44f9a25bc657909685
tan <t.songglod@gmail.com>
2021-10-09 15:35:31 +0700

    [EDIT] add splice() example

--- Assignment3/array_method_6.js
+++ Assignment3/array_method_6.js
@@ -3,7 +3,7 @@ const temp = ["Wed", "Thu", "Fri", "Sat"];
 const days = ["Sun", "Mon", "Tue"];
 
 days.splice(days.length, 0, ...temp);
-// inserts all elements of "temp" at index 3 
+// inserts all elements of "temp" at index 3
 console.log(days);
 
 months.splice(1, 0, "Feb");
@@ -25,3 +25,22 @@ months.splice(1);
 // deletes all elements from index 1
 console.log(months);
 // expected output: Array ["Jan"]
+
+const arr = [2, 6, 8, 15, 9, 12, 5, 4];
+
+function divideArray(arr) {
+	let result = [];
+	if (arr === null || arr === undefined) {
+		return -1;
+	}
+	if (arr.length <= 1) {
+		return arr;
+	} else {
+		let temp = arr.splice(Math.trunc(arr.length / 2));
+		result.push(...temp);
+		return [arr, result];
+	}
+}
+
+console.log(days);
+console.log(divideArray(days));
