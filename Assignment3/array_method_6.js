4f71410deb60573d1db5f57b7a162443592c8685
tan <t.songglod@gmail.com>
2021-10-09 13:35:38 +0700

    [EDIT] add splice() example

--- Assignment3/array_method_6.js
+++ Assignment3/array_method_6.js
@@ -6,6 +6,11 @@
 // splice(start, deleteCount, item1)
 // splice(start, deleteCount, item1, item2, itemN)
 
+// 1. Method Syntax (ทำเฉพาะส่วนที่พารามิเตอร์เป็น primitive types, array, object หรือ  arrow function)
+// 2. คำอธิบายความสามารถของ Method กระชับและชัดเจน
+// 3. วิธีการใช้ Method นั้น ระบุรายการพารามิเตอร์ อธิบายความหมายและชนิดข้อมูล และ output ที่ได้จาก Method
+// 4. ตัวอย่างการใช้งาน ของแต่ละ syntax ให้มีหลาย ๆ กรณีศึกษาที่แตกต่างกันอย่างน้อย 3 ตัวอย่าง
+
 const months = ["Jan", "March", "April", "June"];
 const temp = ["Wed", "Thu", "Fri", "Sat"];
 const days = ["Sun", "Mon", "Tue"];
