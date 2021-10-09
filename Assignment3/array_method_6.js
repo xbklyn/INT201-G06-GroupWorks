4fcf7a049c797336403a0041cd9d0f810f9eba7e
tan <t.songglod@gmail.com>
2021-10-09 13:30:31 +0700

    [EDIT] add splice() example

--- Assignment3/array_method_6.js
+++ Assignment3/array_method_6.js
@@ -6,14 +6,20 @@
 // splice(start, deleteCount, item1)
 // splice(start, deleteCount, item1, item2, itemN)
 
-const months = ['Jan', 'March', 'April', 'June'];
+const months = ["Jan", "March", "April", "June"];
+const temp = ["Wed", "Thu", "Fri", "Sat"];
+const days = ["Sun", "Mon", "Tue"];
 
-months.splice(1, 0, 'Feb');
+days.splice(days.length, 0, ...temp);
+// inserts all elements of "temp" at index 3 
+console.log(days);
+
+months.splice(1, 0, "Feb");
 // inserts at index 1
 console.log(months);
 // expected output: Array ["Jan", "Feb", "March", "April", "June"]
 
-months.splice(4, 1, 'May');
+months.splice(4, 1, "May");
 // replaces 1 element at index 4
 console.log(months);
 // expected output: Array ["Jan", "Feb", "March", "April", "May"]
@@ -24,5 +30,5 @@ console.log(months);
 // expected output: Array ["Jan", "Feb", "May"]
 
 months.splice(1);
-// deletes all elements from index 0
+// deletes all elements from index 1
 console.log(months);
