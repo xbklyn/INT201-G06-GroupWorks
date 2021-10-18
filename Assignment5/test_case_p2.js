93a3ee8d51b4af2821a84eaecf29d670b781b6eb
Kittiphum601 <68773089+Kittiphum601@users.noreply.github.com>
2021-10-18 12:08:53 +0700

    Update test_case_p2.js

--- Assignment5/test_case_p2.js
+++ Assignment5/test_case_p2.js
@@ -4,7 +4,7 @@ import * as lib from "./modules/func_p1.js";
 
 // case 1
 
-const numbers = [2, 6, 8, 1, 9, 12, 5, 4, 6, 5, 0, -5, -9];
+const numbers = [2, 6, 8, 1, 9, 12, 5, 4, 6, 5, 0, -5, -9]; //กำหนดข้อมูลเป็น number
 const testNumbers = lib.default;
 
 console.log(numbers);
@@ -15,32 +15,35 @@ console.log();
 
 let div_arr_function = lib.divArray(numbers);
 let div_arr = div_arr_function(numbers);
+//กำหนดการใช้ function divArray
 console.log(div_arr);
 
 console.log(lib.arrayOperator(div_arr, lib.sumDivArray));
-
+//แสดงผลลัพธ์โดยการใช้ function arrayOperator และ sumDivArray
 
 
 // case 2
 
-const words = ["A", 1, 2, 3, "B", "C"];
+const words = ["A", 1, 2, 3, "B", "C"]; //กำหนดข้อมูลเป็น number และ string
 
 console.log(words);
 console.log();
 
 let div_arr_function = lib.divArray(words);
 let div_arr = div_arr_function(words);
+//กำหนดการใช้ function divArray
 console.log(div_arr);
 
 console.log(lib.arrayOperator(div_arr, lib.sumDivArray));
-
+//แสดงผลลัพธ์โดยการใช้ function arrayOperator และ sumDivArray
 
 
 // case 3
 
-const numbers = [[4, -2, 5, 2, 4], [6, 7, 8]];
+const numbers = [[4, -2, 5, 2, 4], [6, 7, 8]]; //กำหนดค่า array 2 มิติ	
 
 console.log(numbers);
 console.log();
 
 console.log(lib.sumDivArray(numbers));
+//แสดงผลลัพธ์โดยการใช้ function sumDivArray
