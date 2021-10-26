a6a47b95a58670ce57baaef3da6c60b01bc8f017
tan <t.songglod@gmail.com>
2021-10-26 12:10:08 +0700

    [ADD] update template

---
+++ Assignment6/modules/products.js
@@ -0,0 +1,40 @@
+// product management file
+// Theme: Monitor
+// tan add template
+
+class Monitor {
+	constructor(code, name = "unknown", size = 0, stock = 0, resolution = "1440x1080") {
+		this._code = code;
+		this._name = name;
+		this._size = size;
+		this._stock = stock;
+		this._resolution = resolution;
+		this._imgURL = "URL";
+	}
+
+	//set
+	//attributes
+
+	//get
+	//attributes
+
+	//function
+	//add
+	//delete
+}
+
+// const products = [
+// 	{
+// 		code: 0,
+// 		name: "My Product",
+// 		size: 0,
+// 		resolution: "1x1",
+// 		price: 0,
+// 		stock: 0,
+// 		imageURL: "link to image",
+// 	},
+// ];
+
+// add products here
+
+// export { products };
---
+++ Assignment6/modules/script.js
@@ -0,0 +1,7 @@
+// html management file
+
+import * as lib from "./products.js";
+
+// code here
+
+console.log(lib.products);
