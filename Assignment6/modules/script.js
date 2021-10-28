dbd47604ca62e8b576d473bd532ec5b05df4e9e7
Chotiwit Souyan <nut.chotiwit@gmail.com>
2021-10-28 20:53:12 +0700

    [EDIT] add link in html and desc in js

--- Assignment6/modules/script.js
+++ Assignment6/modules/script.js
@@ -1,5 +1,6 @@
 // html management file
 // tan add script
+//nut add description 
 import products from "./products.js";
 
 // code here
@@ -15,7 +16,7 @@ for (const product of products) {
     // add div to container in body
     let divList = document.createElement("div");
     divList.id = product.code;
-    divList.className = "product-item";
+    divList.className = "product-item w-auto";
     let divItem = container.appendChild(divList);
 
     // add img to div in container
@@ -29,20 +30,24 @@ for (const product of products) {
     let divDesc = document.createElement("div");
     
     let name = document.createElement("div");
-    name.class = "name";
+    name.className = "bg-green-100 font-mono flex-1";
     name.innerHTML = `<b>Name</b> : ${product.name} ${product.size}"`;
     divDesc.appendChild(name);
 
     let price = document.createElement("div");
-    price.class = "price";
+    price.className = "";
     price.innerHTML = `<b>Price</b> : ${product.price}`;
     divDesc.appendChild(price);
 
     let res = document.createElement("div");
-    res.class = "resolution";
+    res.className = "";
     res.innerHTML = `<b>Resolution</b> : ${product.resolution}`;
     divDesc.appendChild(res);
 
+    let stock = document.createElement("div");
+    stock.className = ""
+    stock.innerHTML = `<b>In stock</b> : ${product.stock}`;
+    divDesc.appendChild(stock);
 
     divItem.appendChild(divDesc);
 }
