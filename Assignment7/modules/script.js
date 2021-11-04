ab53261f3092afcb2e6552c51c1e714729c776cd
maxuamthon <68773089+maxuamthon@users.noreply.github.com>
2021-11-05 01:07:03 +0700

    Update script.js

--- Assignment7/modules/script.js
+++ Assignment7/modules/script.js
@@ -19,10 +19,6 @@ body.appendChild(title);
 let divContainer = document.createElement("div");
 divContainer.className = "product-container grid grid-cols-5";
 divContainer.id = "store-product"
-
-let divContainer = document.createElement("div");
-divContainer.className = "product-container grid grid-cols-5";
-
 // add container to body
 let container = body.appendChild(divContainer);
 
@@ -30,7 +26,7 @@ for (const product of products) {
 	// add div to container in body
 	let divList = document.createElement("div");
 	divList.id = product.code;
-	divList.className = "flex flex-col m-5 bg-white rounded-lg transition duration-200 ease-in-out hover:bg-purple-200 shodow-xl transform hover:scale-110 ";
+	divList.className = "product";
 	divList.setAttribute("name", product.name);
 	let divItem = container.appendChild(divList);
 
@@ -38,21 +34,19 @@ for (const product of products) {
 	let img = document.createElement("img");
 	img.src = product.imgURL;
 	img.alt = product.name;
-	img.style = "width: 100%; height: 100%; ";
-	img.className = "p-2"
+	img.style = "width: 100%; height: 100%;";
 	divItem.appendChild(img);
 
 	// add details to div in container
 	let divDetails = document.createElement("div");
-	divDetails.className = "details p-2 ";
+	divDetails.className = "details p-2";
 
 	let name = document.createElement("div");
-	name.className = " p-1 bg-green-100 flex-1 m-1";
+	name.className = "bg-green-100 flex-1";
 	name.innerHTML = `<b>Product: </b>${product.name} ${product.size}"`;
 	divDetails.appendChild(name);
 
 	let price = document.createElement("div");
-	price.className = "m-1 p-1"
 	price.innerHTML = `<b>Price: </b>${new Intl.NumberFormat("th-TH", {
 		style: "currency",
 		currency: "THB",
@@ -60,12 +54,10 @@ for (const product of products) {
 	divDetails.appendChild(price);
 
 	let res = document.createElement("div");
-	res.className = "m-1 p-1 ";
 	res.innerHTML = `<b>Resolution: </b>${product.resolution}`;
 	divDetails.appendChild(res);
 
 	let stock = document.createElement("div");
-	stock.className = "m-1 p-1 ";
 	if (product.stock == 0) {
 		let outOfStock = document.createElement("b");
 		outOfStock.className = "text-red-500";
@@ -75,30 +67,19 @@ for (const product of products) {
 		stock.innerHTML = `<b>In stock: </b>${product.stock}`;
 	}
 	divDetails.appendChild(stock);
-	
+
 	divItem.appendChild(divDetails);
-	
+}
 
-	//"Add" button 
-	let divBtn = document.createElement("div");
-	divBtn.className = "m-1 p-1";
-	let btn = document.createElement("button");
-	btn.className = `w-full p-2 rounded ${product.stock == 0 ? "bg-gray-100 text-gray-300 pointer-events-none" : "bg-purple-400 text-white transition duration-150 ease-in-out"}`;
-	btn.innerHTML = `<b><i>Add</i></b>`;
-	divBtn.appendChild(btn);
-	divDetails.appendChild(divBtn);
-	
-
-//toggle search icon
 document.querySelector(".icon").addEventListener("click", function () {
 	document.querySelector("input").classList.toggle("active");
 })
 
-//search function
 const productStore = document.querySelectorAll(".product");
 const search = document.getElementById("search");
 
 search.addEventListener("keyup", filterProducts);
+
 
 function filterProducts(e) {
 	const text = e.target.value.toLowerCase();
@@ -111,4 +92,3 @@ function filterProducts(e) {
 		}
 	}
 }
-
