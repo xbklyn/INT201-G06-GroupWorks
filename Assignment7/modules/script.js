9b6ee063507a01853fe6782b00fc7013de65946c
xbklyn <chotiwit.souyan@mail.kmutt.ac.th>
2021-11-05 00:23:32 +0700

    [EDIT] update css and add 'add' button but no any event handler yet.

--- Assignment7/modules/script.js
+++ Assignment7/modules/script.js
@@ -26,7 +26,7 @@ for (const product of products) {
 	// add div to container in body
 	let divList = document.createElement("div");
 	divList.id = product.code;
-	divList.className = "flex flex-col bg-white rounded-lg shadow-md m-5";
+	divList.className = "flex flex-col m-5 bg-white rounded-lg transition duration-200 ease-in-out hover:bg-purple-200 shodow-xl transform hover:scale-110 ";
 	divList.setAttribute("name", product.name);
 	let divItem = container.appendChild(divList);
 
@@ -34,19 +34,21 @@ for (const product of products) {
 	let img = document.createElement("img");
 	img.src = product.imgURL;
 	img.alt = product.name;
-	img.style = "width: 100%; height: 100%;";
+	img.style = "width: 100%; height: 100%; ";
+	img.className = "p-2"
 	divItem.appendChild(img);
 
 	// add details to div in container
 	let divDetails = document.createElement("div");
-	divDetails.className = "details p-2";
+	divDetails.className = "details p-2 ";
 
 	let name = document.createElement("div");
-	name.className = "bg-green-100 flex-1";
+	name.className = " p-1 bg-green-100 flex-1 m-1";
 	name.innerHTML = `<b>Product: </b>${product.name} ${product.size}"`;
 	divDetails.appendChild(name);
 
 	let price = document.createElement("div");
+	price.className = "m-1 p-1"
 	price.innerHTML = `<b>Price: </b>${new Intl.NumberFormat("th-TH", {
 		style: "currency",
 		currency: "THB",
@@ -54,10 +56,12 @@ for (const product of products) {
 	divDetails.appendChild(price);
 
 	let res = document.createElement("div");
+	res.className = "m-1 p-1 ";
 	res.innerHTML = `<b>Resolution: </b>${product.resolution}`;
 	divDetails.appendChild(res);
 
 	let stock = document.createElement("div");
+	stock.className = "m-1 p-1 ";
 	if (product.stock == 0) {
 		let outOfStock = document.createElement("b");
 		outOfStock.className = "text-red-500";
@@ -68,5 +72,16 @@ for (const product of products) {
 	}
 	divDetails.appendChild(stock);
 
+	//"Add" button 
+	let divBtn = document.createElement("div");
+	divBtn.className = "m-1 p-1";
+	let btn = document.createElement("button");
+	btn.className = `w-full p-2 rounded ${product.stock == 0 ? "bg-gray-100 text-gray-300 pointer-events-none" : "bg-purple-400 text-white transition duration-150 ease-in-out"}`;
+	btn.innerHTML = `<b><i>Add</i></b>`;
+	divBtn.appendChild(btn);
+	divDetails.appendChild(divBtn);
+	
+
 	divItem.appendChild(divDetails);
 }
+
