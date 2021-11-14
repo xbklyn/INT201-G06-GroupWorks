3245e02f3c3543c86b8eea3e97868c39bec80724
maxuamthon <68773089+maxuamthon@users.noreply.github.com>
2021-11-14 15:18:39 +0700

    Update cookie.js

--- Assignment8/modules/cookie.js
+++ Assignment8/modules/cookie.js
@@ -2,7 +2,7 @@ function setCookie(cname, cvalue, exdays) {
 	const d = new Date();
 	d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
 	let expires = "expires=" + d.toGMTString();
-	document.cookie = cname + "" + cvalue + "" + expires + "";
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
 function getCookie(cname) {
