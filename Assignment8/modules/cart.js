6cb872dcd4c02111869084501183ad9ba523d7e2
tan <t.songglod@gmail.com>
2021-11-15 18:13:28 +0700

    [EDIT] completed!!!

--- Assignment8/modules/cart.js
+++ Assignment8/modules/cart.js
@@ -9,7 +9,6 @@ const cart = {
 	netPrice: 0,
 	totalQuantity: 0,
 };
-CookieUtil.setCookie("shopping_cart", JSON.stringify(cart, 0, 2), 1);
 
 // new features
 // remove: function (id) {},
@@ -19,32 +18,36 @@ CookieUtil.setCookie("shopping_cart", JSON.stringify(cart, 0, 2), 1);
  * @param {number} id id of the button
  */
 function add(id) {
-	let sc = JSON.parse(CookieUtil.getCookie("shopping_cart"));
-	if (sc.itemID.includes(id + 1)) {
-		const index = sc.itemID.indexOf(id + 1);
-		sc.items[index].quantity += 1;
-		let price = sc.items[index].price;
-		let quantity = sc.items[index].quantity;
-		sc.items[index].totalPrice = price * quantity;
-		sc.totalQuantity += 1;
+	if (CookieUtil.getCookie("shopping_cart") === "") {
+		CookieUtil.setCookie("shopping_cart", JSON.stringify(cart, 0, 2), 1);
 	} else {
-		sc.itemID.push(id + 1);
-		sc.items.push({
-			productDetails: {
-				img: products[id].imgURL,
-				productCode: products[id].code,
-				productName: products[id].name,
-				resolution: products[id].resolution,
-				sizeInInches: products[id].size,
-			},
-			price: products[id].price,
-			quantity: 1,
-			totalPrice: products[id].price,
-		});
-		sc.totalQuantity += 1;
+		let sc = JSON.parse(CookieUtil.getCookie("shopping_cart"));
+		if (sc.itemID.includes(id + 1)) {
+			const index = sc.itemID.indexOf(id + 1);
+			sc.items[index].quantity += 1;
+			let price = sc.items[index].price;
+			let quantity = sc.items[index].quantity;
+			sc.items[index].totalPrice = price * quantity;
+			sc.totalQuantity += 1;
+		} else {
+			sc.itemID.push(id + 1);
+			sc.items.push({
+				productDetails: {
+					img: products[id].imgURL,
+					productCode: products[id].code,
+					productName: products[id].name,
+					resolution: products[id].resolution,
+					sizeInInches: products[id].size,
+				},
+				price: products[id].price,
+				quantity: 1,
+				totalPrice: products[id].price,
+			});
+			sc.totalQuantity += 1;
+		}
+		sc.netPrice += products[id].price;
+		CookieUtil.setCookie("shopping_cart", JSON.stringify(sc, 0, 2), 1);
 	}
-	sc.netPrice += products[id].price;
-	CookieUtil.setCookie("shopping_cart", JSON.stringify(sc, 0, 2), 1);
 }
 
 export { cart, add };
